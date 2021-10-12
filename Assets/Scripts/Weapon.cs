fc06c7635a79edb899e3dbe83dd2e76b8b9f2773
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-12 20:29:43 +0100

    Debug raycasting added

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float raycastRange = 100f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, raycastRange);
+        Debug.Log("I hit this thing: " + hit.transform.name);
+    }
+}
