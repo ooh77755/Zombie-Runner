bfdbe6a63caac9930ca029b658e035b94f4f87eb
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-18 20:07:47 +0100

    Triggered animations through code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -50,11 +50,14 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", true);
         Debug.Log("Enemy is attacking");
     }
 
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 
