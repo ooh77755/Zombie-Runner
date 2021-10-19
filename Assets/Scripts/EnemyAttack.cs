f4bf47bdbc5db638a349783dbf82756c4df78d03
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-19 03:57:30 +0100

    Optimised player health/enemy damage code (with Rick's input)

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    PlayerHealth target;
     [SerializeField] float enemyDamage = 40f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerHealth>();
     }
     
     public void AttackHitEvent()
     {
-        PlayerHealth targetPH = FindObjectOfType<PlayerHealth>();
         if (target == null)
         {
             return;
         }
-        targetPH.ReceiveDamage(enemyDamage);
+        target.ReceiveDamage(enemyDamage);
         Debug.Log("Enemy hits Player");
     }
 }
