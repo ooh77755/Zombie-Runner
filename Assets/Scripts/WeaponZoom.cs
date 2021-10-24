eb3f15553d8ec3d8c50fa7efa41adb2049d6fddc
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-24 20:53:38 +0100

    Toggled mouse sensitivity on zoom in/zoom out

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
     float zoomedInFOV = 20f;
     float zoomedOutFOV = 60f;
+    RigidbodyFirstPersonController rbScript;
+    float ZoomOut = 2f;
+    float ZoomIn = 0.5f;
 
     bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        rbScript = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,12 +28,16 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 mainCamera.fieldOfView = zoomedInFOV;
+                rbScript.mouseLook.XSensitivity = ZoomIn;
+                rbScript.mouseLook.YSensitivity = ZoomIn;
             }
 
             else
             {
                 zoomedInToggle = false;
                 mainCamera.fieldOfView = zoomedOutFOV;
+                rbScript.mouseLook.XSensitivity = ZoomOut;
+                rbScript.mouseLook.YSensitivity = ZoomOut;
             }
         }
     }
