292d999010b9f53a5e0197f8d364aea127776a9b
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-24 18:12:12 +0100

    Added code to toggle zoomed in/out FOV for main camera when aiming

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    float zoomedInFOV = 20f;
+    float zoomedOutFOV = 60f;
+
+    bool zoomedInToggle = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                mainCamera.fieldOfView = zoomedInFOV;
+            }
+
+            else
+            {
+                zoomedInToggle = false;
+                mainCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
