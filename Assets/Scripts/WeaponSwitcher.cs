2bf48f6640885a8debabc348b815b6de58c41725
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-11-02 18:30:55 +0000

    Created 'weaponSwitcher.cs' and foreach loop & weaponIndex, so that engine could know which gun from player's arsenal was meant to be active at any one time

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+        SetActiveWeapon();
+    }
+
+    private void SetActiveWeapon()
+    {
+        int weaponIndex = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+
+            else if (weaponIndex != currentWeapon)
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
